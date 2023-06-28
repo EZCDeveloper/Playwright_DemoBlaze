ab72a5f76b4add856751aed533566eca2a2adcfd
Emanuel Zini Casaro <emanuel.zini-casaro@appdirect.com>
2023-06-28 17:19:14 -0300

    added beforeEach hook

--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -1,9 +1,11 @@
 // @ts-check
 const { test, expect } = require('@playwright/test')
 
-test('TC_01. has title and logo name', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('https://demoblaze.com/')
+})
 
+test('TC_01. has title and logo name', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/STORE/)
 
@@ -13,16 +15,12 @@ test('TC_01. has title and logo name', async ({ page }) => {
 })
 
 test('TC_02. has navbar', async ({ page }) => {
-  await page.goto('https://demoblaze.com/')
-
   // Expect navbar exist
   const navBar = page.locator('#navbarExample')
   await expect(navBar).toBeVisible()
 })
 
 test('TC_03. navbar contains Home text', async ({ page }) => {
-  await page.goto('https://demoblaze.com/')
-
   // Expect navbar contains home text
   const homeNav = page.getByRole('link', { name: 'Home (current)' })
   await expect(homeNav).toHaveText(['Home (current)'])
